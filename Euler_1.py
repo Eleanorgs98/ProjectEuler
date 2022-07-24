183d6cea603eb1000432df37af34b916a3c2660b
sam feltondobbs <elliegs1@hotmail.co.uk>
2022-07-24 18:11:11 +0100

    Remembered to save code

--- Euler_1.py
+++ Euler_1.py
@@ -4,3 +4,11 @@ Created on Sun Jul 24 15:00:05 2022
 
 @author: Eleanor Glyn-Smith
 """
+total = 0
+for i in range(1000):
+    if (i%3 == 0) or (i%5 == 0):
+        total = total +i
+        
+print(total)
+
+        
