8409434d66ca68b6d217c09c075dbc3113995a78
sam feltondobbs <elliegs1@hotmail.co.uk>
2022-08-06 16:53:53 +0100

    Problem 8

---
+++ Euler_8.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  5 12:16:21 2022
+
+@author: Eleanor Glyn-Smith
+"""
+# function which takes thirteen consecutive digits from a string and multiplies them together
+# needs to find the maximum product
